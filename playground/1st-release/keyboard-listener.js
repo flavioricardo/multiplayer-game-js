98639668e5c941555560eff9cf202797fb5d2b5f
Flávio Ricardo <flavioricardo@live.com>
2022-01-19 03:19:24 +0000

    Finalizando o Front-End organizando as camadas em módulos

---
+++ playground/1st-release/keyboard-listener.js
@@ -0,0 +1,37 @@
+// Input layer
+export default function createKeyboardListener(document) {
+  const state = {
+    observers: [],
+  };
+
+  function subscribe(observerFunction) {
+    state.observers.push(observerFunction);
+  }
+
+  function notifyAll(command) {
+    console.log(
+      `keyboardListener -> Notifying ${state.observers.length} observers`
+    );
+
+    for (const observerFunction of state.observers) {
+      observerFunction(command);
+    }
+  }
+
+  document.addEventListener("keydown", handleKeyDown);
+
+  function handleKeyDown(event) {
+    const keyPressed = event.key;
+
+    const command = {
+      playerId: "player1",
+      keyPressed,
+    };
+
+    notifyAll(command);
+  }
+
+  return {
+    subscribe,
+  };
+}
